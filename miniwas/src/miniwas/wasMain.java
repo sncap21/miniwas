ba6f9b7dfb69d6f73f068dd18aa17795dd77d55f
sncap21 <40551530+sncap21@users.noreply.github.com>
2018-07-10 10:08:20 +0900

    1st commit

---
+++ miniwas/src/miniwas/wasMain.java
@@ -0,0 +1,33 @@
+package miniwas;
+
+public class wasMain {
+
+	public static void main(String[] args) {
+
+		//1. property file load
+
+		
+		
+		
+		
+		
+		//2. socket create & accept
+		
+		//3. request 요청 시
+		    // 3.1 request, response 내용 채우기 
+			// 3.2 request mapping
+		    // 3.3 servlet thread 실행
+		   
+		//4. servlet thread 실행 
+		   // 4.1 init
+		   // 4.2 service  : 
+		   // 4.3 doGet, doPost
+		   // 4.4 destory : thread delete
+		
+		
+
+	}
+
+}
+
+
